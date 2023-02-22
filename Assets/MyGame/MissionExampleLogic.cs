095cf01a7955fc4a205425e95d353aadac173b8b
DmitriyKovriga <120663554+DmitriyKovriga2022@users.noreply.github.com>
2023-02-22 03:21:30 +0300

    980

--- Assets/MyGame/MissionExampleLogic.cs
+++ Assets/MyGame/MissionExampleLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using Enums;
+using System;
 
 public class MissionExampleLogic : MissionBaseLogic
 {
@@ -16,49 +17,150 @@ public class MissionExampleLogic : MissionBaseLogic
 
     public override void Init()
     {
-        var missionTimer = Controller.CreateTimer();
-        missionTimer.TimerFinish += MissionTimeFinished; // функция лежит в MissionBaseLogic
-        missionTimer.Start(50f);
-        
-        Controller.SpawnSquad("Soldier", 1, 2, "Covers/cover2");
-		Controller.SpawnSquad("Soldier", 1, 2, "Covers/cover1", "Covers/cover3");
-		
         var button1 = Controller.GetButton("Button1");
         button1.Pressed += Button1OnPressed;
+        var trigger = Controller.GetTrigger("Trigger1");
+        trigger.Enabled = true;
+        trigger.Entered += OnTrigger1Enter;
+
+        
+
+
     }
 
+    
+    //----------- evacuation logic -------------
     void OnBossTimeEnded()
     {
         Controller.FinishMission(FinishReason.Defeat);
     }
+    //----------- evacuation logic -------------
 
-    void CarDestroy()
+    void CarDestroy(UnityEngine.Object car)
     {
-        Controller.DestroyObject(Car1);
+        Controller.DestroyObject(car);
     }
     
     void Button1OnPressed()
     {
-        var squad1 = Controller.SpawnSquad("Sniper", 1, 0, "Covers/cover4");
+        
+    }
+
+    void OnTrigger1Enter ()
+    {
+        var squad1 = CreateAndSpawnSquad(
+                                 "Gunner",
+                                 "Gunner",
+                                 "Soldier",
+                                 "Soldier",
+                                 "Soldier",
+                                 "Sniper",
+                                    "cover1",
+                                    "cover2",
+                                    "cover3",
+                                    "cover4",
+                                    "cover5",
+                                    "cover6"
+                                 );
+        Controller.GetTrigger("Trigger1").Enabled = false;
         squad1.AllSquadDead += OnSquad1Dead;
     }
 
     void OnSquad1Dead(Squad squad)
     {
-        var squad1 = Controller.SpawnSquad("Gunner", 1, 0, "Covers/cover5");
-        squad1.BotKilled += OnBossKilled;
-
-        Car1 = Controller.SpawnObject("Car", new Vector3(-14.72f, 0.7f, -22.376f));
-		//Car1 = Controller.SpawnObject("Car", "CarPoint");
-
-        var timer = Controller.CreateTimer();
-        timer.TimerFinish += OnBossTimeEnded;
-        timer.Start(8f);
+        Debug.Log("Door open");
     }
 
-    void OnBossKilled(Bot bot)
+
+    //Создал метод для удобного создания своих отрядов. Переопределил его ради гибкости использования. Присутствует повторение кода, но на мой взгляд это меньшее из зол.
+
+    Squad CreateAndSpawnSquad(string enemyType1, string enemyType2, string enemyType3, string enemyType4,
+        string cover1, string cover2, string cover3, string cover4)
     {
-        CarDestroy();
-        Controller.FinishMission(FinishReason.Victory);
+        Squad squad1 = Controller.SpawnSquad(enemyType1, 1, 0, cover1);
+        try
+        {
+            squad1.Bots.AddRange(Controller.SpawnSquad(enemyType2, 1, 0, cover3).Bots);
+            squad1.Bots.AddRange(Controller.SpawnSquad(enemyType3, 1, 0, cover3).Bots);
+            squad1.Bots.AddRange(Controller.SpawnSquad(enemyType4, 1, 0, cover4).Bots);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Не критичная ошибка: " + e);
+        }
+
+        Debug.Log(squad1.Bots);
+        return squad1;
+    }
+
+        Squad CreateAndSpawnSquad(string enemyType1, string enemyType2, string enemyType3, string enemyType4, string enemyType5,
+        string cover1, string cover2, string cover3, string cover4, string cover5)
+    {
+        Squad squad1 = Controller.SpawnSquad(enemyType1, 1, 0, cover1);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType2, 1, 0, cover2).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType3, 1, 0, cover3).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType4, 1, 0, cover4).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType5, 1, 0, cover5).Bots);
+        return squad1;
+    }
+
+    Squad CreateAndSpawnSquad(string enemyType1, string enemyType2, string enemyType3, string enemyType4, string enemyType5, string enemyType6,
+        string cover1, string cover2, string cover3, string cover4, string cover5, string cover6)
+    {
+        Squad squad1 = Controller.SpawnSquad(enemyType1, 1, 0, cover1);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType2, 1, 0, cover2).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType3, 1, 0, cover3).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType4, 1, 0, cover4).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType5, 1, 0, cover5).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType6, 1, 0, cover6).Bots);
+        Debug.Log(squad1.Bots);
+        return squad1;
+    }
+
+    Squad CreateAndSpawnSquad(string enemyType1, string enemyType2, string enemyType3, string enemyType4, string enemyType5, string enemyType6, string enemyType7,
+        string cover1, string cover2, string cover3, string cover4, string cover5, string cover6, string cover7)
+    {
+        Squad squad1 = Controller.SpawnSquad(enemyType1, 1, 0, cover1);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType2, 1, 0, cover2).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType3, 1, 0, cover3).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType4, 1, 0, cover4).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType5, 1, 0, cover5).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType6, 1, 0, cover6).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType7, 1, 0, cover7).Bots);
+        return squad1;
+    }
+
+    Squad CreateAndSpawnSquad(string enemyType1, string enemyType2, string enemyType3, string enemyType4, string enemyType5, string enemyType6, string enemyType7, string enemyType8,
+        string cover1, string cover2, string cover3, string cover4, string cover5, string cover6, string cover7, string cover8)
+    {
+        Squad squad1 = new Squad(enemyType1, 1, 0, cover1);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType2, 1, 0, cover2).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType3, 1, 0, cover3).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType4, 1, 0, cover4).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType5, 1, 0, cover5).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType6, 1, 0, cover6).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType7, 1, 0, cover7).Bots);
+        squad1.Bots.AddRange(Controller.SpawnSquad(enemyType8, 1, 0, cover8).Bots);
+        return squad1;
     }
 }
+
+
+
+//example
+/*Controller.SpawnSquad("Soldier", 1, 2, "Covers/cover2");
+Controller.SpawnSquad("Soldier", 1, 2, "Covers/cover1", "Covers/cover3");*/
+
+/*Car1 = Controller.SpawnObject("Car", new Vector3(-14.72f, 0.7f, -22.376f));
+Car1 = Controller.SpawnObject("Car", "CarPoint");*/
+
+
+//----------- evacuation logic -------------
+/*var missionTimer = Controller.CreateTimer();
+    missionTimer.TimerFinish += MissionTimeFinished; // функция лежит в MissionBaseLogic
+    missionTimer.Start(50f);*/
+
+/*var timer = Controller.CreateTimer();
+timer.TimerFinish += OnBossTimeEnded;
+timer.Start(8f);*/
+//----------- evacuation logic -------------
